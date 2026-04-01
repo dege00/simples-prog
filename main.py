6b4046f84f017433202662f49970f2a83c75a409
dege00 <fontenelediego00@gmail.com>
2026-03-31 23:15:50 -0300

    programa-simples

---
+++ main.py
@@ -0,0 +1,6 @@
+primeira nota = float(input("Digite a primeira nota: "))
+segunda nota = float(input("Digite a segunda nota: "))
+
+media = (primeira nota + segunda nota) / 2
+
+print(f"A média final é
